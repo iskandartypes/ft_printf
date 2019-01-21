599f4f8795da5fef90e155c3dc87e391bc044882
naregjan <iskandar.kourkjian.mowad@gmail.com>
2019-01-21 14:10:40 -0800

    init, adding libft although might remove later

---
+++ ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikourkji <ikourkji@student.42.us.or>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/20 01:52:18 by ikourkji          #+#    #+#             */
+/*   Updated: 2019/01/20 02:00:59 by ikourkji         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "./libft/libft.h"
+
+int	printf(const char *format, ...);
+
+#endif
