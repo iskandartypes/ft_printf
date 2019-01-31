727d143c6b72815b52c30743fffbfa7ec6a0a738
naregjan <iskandar.kourkjian.mowad@gmail.com>
2019-01-31 05:05:03 -0800

    now does basic char and str, not the wide ones yet tho

--- pf_strings.c
+++ pf_strings.c
@@ -6,7 +6,7 @@
 /*   By: ikourkji <ikourkji@student.42.us.or>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/31 02:36:16 by ikourkji          #+#    #+#             */
-/*   Updated: 2019/01/31 02:48:50 by ikourkji         ###   ########.fr       */
+/*   Updated: 2019/01/31 05:04:42 by ikourkji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,49 @@
 
 void	pf_float(t_vars *v)
 {
+	f->flags & F_PREC ? 0 : f->prec = 6;
 	printf("this is for floating nums\n");
 }
 
 void	pf_char(t_vars *v)
 {
-	printf("this is for chars\n");
+	unsigned char	c;
+
+	if (v->flags & F_L)
+	{}
+	else
+	{
+		c = va_arg(v->args, int);
+		v->min--;
+		if (!(v->flags & F_RPAD))
+			while (v->min-- > 0)
+				pf_placechar(v, v->flags & F_ZPAD ? '0' : ' ');
+		pf_placechar(v, c);
+		while (v->min-- > 0)
+			pf_placechar(v, ' ');
+	}
 }
 
 void	pf_str(t_vars *v)
 {
-	printf("this is for strs\n");
+	char	*s;
+	int		i;
+
+	if (v->flags & F_L)
+	{}
+	else
+	{
+		s = va_arg(v->args, char*);
+		v->clen = ft_strlen(s);
+		(v->flags & F_PREC) && (v->clen > v->prec) ? v->clen = v->prec : 0;
+		v->min -= v->clen;
+		if (!(v->flags & F_RPAD))
+			while (v->min-- > 0)
+				pf_placechar(v, v->flags & F_ZPAD ? '0' : ' ');
+		i = 0;
+		while (i < v->clen)
+			pf_placechar(v, s[i++]);
+		while (v->min-- > 0)
+			pf_placechar(v, ' ');
+	}
 }
